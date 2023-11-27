deb577dab92756231de5f484da791529c0bc8ff2
Gregory Rodriguez <grodriguez13@myseneca.ca>
2023-11-26 23:38:35 -0500

    add w9p2

--- workshops/ws9/process_data.cpp
+++ workshops/ws9/process_data.cpp
@@ -12,6 +12,8 @@
 #include <thread>
 #include <vector>
 
+#include "process_data.h"
+
 namespace sdds {
 // The following function receives array (pointer) as the first argument, number of array
 //   elements (size) as second argument, divisor as the third argument, and avg as fourth argument.
@@ -47,11 +49,7 @@ void computeVarFactor(const int* arr, int size, int divisor, double avg, double&
 //   to hold the data items, and reads the data items into the allocated memory space.
 //   It prints first five data items and the last three data items as data samples.
 //
-ProcessData::ProcessData(const std::string& filename) {
-    // TODO: Open the file whose name was received as parameter and read the content
-    //         into variables "total_items" and "data". Don't forget to allocate
-    //         memory for "data".
-    //       The file is binary and has the format described in the specs.
+ProcessData::ProcessData(const std::string& filename, int n_threads) {
     std::ifstream file(filename, std::ios::binary);
     if (!file.is_open()) {
         throw std::ios_base::failure("Cannot open file: " + filename);
@@ -66,29 +64,93 @@ ProcessData::ProcessData(const std::string& filename) {
     std::cout << "  [" << data[0] << ", " << data[1] << ", " << data[2] << ", ... , "
               << data[total_items - 3] << ", " << data[total_items - 2] << ", "
               << data[total_items - 1] << "]" << std::endl;
+    // Following statements initialize the variables added for multi-threaded
+    //   computation
+    num_threads = n_threads;
+    averages = new double[num_threads]{};
+    variances = new double[num_threads]{};
+    p_indices = new int[num_threads + 1]{};
+    for (int i = 0; i < num_threads; i++)
+        p_indices[i] = i * (total_items / num_threads);
+    p_indices[num_threads] = total_items;
 }
 
 ProcessData::~ProcessData() {
     delete[] data;
+    delete[] averages;
+    delete[] variances;
+    delete[] p_indices;
 }
 
 ProcessData::operator bool() const {
-    return total_items > 0 && data != nullptr;
+    return total_items > 0 && data;
 }
 
-// TODO You create implementation of function operator(). See workshop instructions for details.
+// TODO Improve operator() function from part-1 for multi-threaded operation. Enhance the
+//   function logic for the computation of average and variance by running the
+//   function computeAvgFactor and computeVarFactor in multile threads.
+// The function divides the data into a number of parts, where the number of parts is
+//   equal to the number of threads. Use multi-threading to compute average-factor for
+//   each part of the data by calling the function computeAvgFactor. Add the obtained
+//   average-factors to compute total average. Use the resulting total average as the
+//   average value argument for the function computeVarFactor, to compute variance-factors
+//   for each part of the data. Use multi-threading to compute variance-factor for each
+//   part of the data. Add computed variance-factors to obtain total variance.
+// Save the data into a file with filename held by the argument fname_target.
+// Also, read the workshop instruction.
 int ProcessData::operator()(const std::string& target_file, double& avg, double& var) {
-    computeAvgFactor(data, total_items, total_items, avg);
-    computeVarFactor(data, total_items, total_items, avg, var);
+    std::vector<std::thread> threads;
+    // Compute avg using multiple threads
+    auto bindComputeAvg = std::bind(&computeAvgFactor, std::placeholders::_1, std::placeholders::_2, total_items, std::placeholders::_3);
+    for (int i = 0; i < num_threads; i++) {
+        threads.emplace_back(std::ref(bindComputeAvg), data + p_indices[i], p_indices[i + 1] - p_indices[i], std::ref(averages[i]));
+    }
+    for (std::thread& thread : threads) {
+        thread.join();
+    }
+    for (int i = 0; i < num_threads; i++) {
+        avg += averages[i];
+    }
+    avg /= num_threads;
+    threads.clear();
+    // Compute var using multiple threads
+    auto bindComputeVar = std::bind(&computeVarFactor, std::placeholders::_1, std::placeholders::_2, total_items, avg, std::placeholders::_3);
+    for (int i = 0; i < num_threads; i++) {
+        threads.emplace_back(std::ref(bindComputeVar), data + p_indices[i], p_indices[i + 1] - p_indices[i], std::ref(variances[i]));
+    }
+    for (std::thread& thread : threads) {
+        thread.join();
+    }
+    for (int i = 0; i < num_threads; i++) {
+        var += variances[i];
+    }
+    // Write to target_file
     std::ofstream file(target_file, std::ios::binary);
     if (!file.is_open()) {
         throw std::ios_base::failure("Cannot open file: " + target_file);
     }
-	file.write(reinterpret_cast<char*>(&total_items), sizeof(int));
-	for (int i = 0; i < total_items; i++) {
-		file.write(reinterpret_cast<char*>(&data[i]), sizeof(int));
-	}
-	file.close();
-	return 0;
+    file.write(reinterpret_cast<char*>(&total_items), sizeof(int));
+    for (int i = 0; i < total_items; i++) {
+        file.write(reinterpret_cast<char*>(&data[i]), sizeof(int));
+    }
+    file.close();
+    return 0;
 }
+
+// Part 1
+// TODO You create implementation of function operator(). See workshop instructions for details.
+// int ProcessData::operator()(const std::string& target_file, double& avg, double& var) {
+//     computeAvgFactor(data, total_items, total_items, avg);
+//     computeVarFactor(data, total_items, total_items, avg, var);
+//     std::ofstream file(target_file, std::ios::binary);
+//     if (!file.is_open()) {
+//         throw std::ios_base::failure("Cannot open file: " + target_file);
+//     }
+//     file.write(reinterpret_cast<char*>(&total_items), sizeof(int));
+//     for (int i = 0; i < total_items; i++) {
+//         file.write(reinterpret_cast<char*>(&data[i]), sizeof(int));
+//     }
+//     file.close();
+//     return 0;
+// }
 }  // namespace sdds
--- workshops/ws9/process_data.h
+++ workshops/ws9/process_data.h
@@ -15,9 +15,15 @@ void computeVarFactor(const int* arr, int size, int divisor, double avg, double&
 class ProcessData {
     int total_items{};
     int* data{};
+    // Following parameters are included to be used for managing multi-threaded
+    //  computation in the operator() function.
+    int num_threads{0};          // to hold number of threads
+    double* averages{nullptr};   // to hold average factors
+    double* variances{nullptr};  // to hold variance factors
+    int* p_indices{nullptr};     // to hold partitioning indices
 
    public:
-    ProcessData(const std::string& input_file);
+    ProcessData(const std::string& input_file, int n_threads);
     ProcessData(const ProcessData&) = delete;
     ProcessData& operator=(const ProcessData&) = delete;
     ~ProcessData();
@@ -25,5 +31,4 @@ class ProcessData {
     int operator()(const std::string& target_file, double& avg, double& var);
 };
 }  // namespace sdds
-
 #endif
